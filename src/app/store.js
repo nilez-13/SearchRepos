e361188ab05099136dc2f49cfbc8cb234d37f403
Nilej <nilez.sth@gmail.com>
2022-11-12 12:28:06 +0545

    cleanup

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import searchReducer from "../containers/Search/searchSlice";
 import detailReducer from "../containers/Detail/detailSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     search: searchReducer,
     detail: detailReducer,
   },
